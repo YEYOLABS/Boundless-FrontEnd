ea51a4100dc3696f86d4c1bf4bf0ac8c3557ac26
Lameck Ndhlovu <lameckndhlovu@Lamecks-MacBook-Air.local>
2026-02-04 14:10:47 +0200

    with latest changes and netlify.toml

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App.tsx';
 
 // Removed manual process.env polyfill to comply with Gemini API coding guidelines.
